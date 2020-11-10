eae4699ceec76aa848bb3fa51c499daa96ee4d36
Long <phannguyenlong0812@gmail.com>
2020-11-10 10:12:42 +0100

    init: create maven project

---
+++ calendar/src/main/java/java_calendar/App.java
@@ -0,0 +1,8 @@
+package java_calendar;
+
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println(Double.parseDouble("0.1"));
+    }
+}
---
+++ calendar/src/main/java/java_calendar/Main.java
@@ -0,0 +1,7 @@
+package java_calendar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello this si main");
+    }
+}
