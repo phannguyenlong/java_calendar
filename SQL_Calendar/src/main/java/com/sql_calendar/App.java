5a3d42a56e9157b2e578fff28128706227a599f6
Long <phannguyenlong0812@gmail.com>
2020-11-13 10:41:52 +0100

    update: link with jdbc driver for sql server

---
+++ SQL_Calendar/src/main/java/com/sql_calendar/App.java
@@ -0,0 +1,48 @@
+package com.sql_calendar;
+
+import java.sql.Connection;
+import java.sql.Statement;
+// import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+
+public class App {
+    private static String DB_URL = "jdbc:sqlserver://localhost:1433;"
+        + "databaseName=Test;";
+    private static String USER_NAME = "sa";
+    private static String PASSWORD = "123";
+
+    public static Connection getConnection(String dbURL, String userName, 
+            String password) {
+        Connection conn = null;
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection(dbURL, userName, password);
+            System.out.println("connect successfully!");
+        } catch (Exception ex) {
+            System.out.println("connect failure!");
+            ex.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        try {
+            // connnect to database 'testdb'
+            Connection conn = getConnection(DB_URL, USER_NAME, PASSWORD);
+            // crate statement
+            Statement stmt = conn.createStatement();
+            // get data from table 'student'
+            ResultSet rs = stmt.executeQuery("select * from student");
+            // show data
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) 
+                        + "  " + rs.getString(3));
+            }
+            // close connection
+            conn.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
