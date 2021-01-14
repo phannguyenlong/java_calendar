327f4c3b8677940c0da09d03632b6e8706bea96f
Long <phannguyenlong0812@gmail.com>
2021-01-14 23:24:25 +0100

    update: basic design for week view

--- SQL_Calendar/src/main/java/com/sql_calendar/controller/manager/WeekViewController.java
+++ SQL_Calendar/src/main/java/com/sql_calendar/controller/manager/WeekViewController.java
@@ -1,13 +1,36 @@
 package com.sql_calendar.controller.manager;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-import javafx.fxml.Initializable;
+import com.sql_calendar.resources.EventInstance;
+import com.sql_calendar.util.GetRequestModel;
 
-public class WeekViewController implements Initializable{
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+public class WeekViewController implements Initializable {
+    @FXML
+    VBox monday, tuesday, webnesday, thursday, friday, saturday, sunday;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Week View");
+
+        GetRequestModel request = new GetRequestModel();
+        Thread makeRequest = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                ArrayList<EventInstance> datas = request.makeRequest("/manager/calendar/week", EventInstance.class,
+                        "date=" + "1/5/2021");
+                for (EventInstance data : datas) {
+                    System.out.println(data);
+                    // TODO: derived data into 7 arrays, then sort 7 array by startTime and ID (if same startTime, compare ID). Use Comparable
+                }
+            }
+        });
+        makeRequest.run();
     }
 }
