b4bb145338f9b20998e3b244191a421fec8d2bcd
HP <HP@KenJonnidip>
2021-01-25 12:14:04 +0100

    change fuck to box
    set delete button

--- SQL_Calendar/src/main/java/com/sql_calendar/controller/cashier/HBoxOrderDelController.java
+++ SQL_Calendar/src/main/java/com/sql_calendar/controller/cashier/HBoxOrderDelController.java
@@ -17,17 +17,23 @@ public class HBoxOrderDelController implements Initializable {
 	@FXML 
 	Label priceTag;
 	@FXML
-	HBox fuck;
+	HBox box;
 	
 	// Cashing VBox
 	private VBox parentVbox;
 	
 	// call CashingController
 	private CashingController parentController;
+	
+	private NewItemPopupController popupController;
 
 	// setParentVbox
     public void setParentVbox(VBox box) {
     	this.parentVbox = box;
+    }
+    
+    public void setPopupController(NewItemPopupController controller) {
+    	this.popupController = controller;
     }
     
     // setParentController
@@ -37,26 +43,23 @@ public class HBoxOrderDelController implements Initializable {
     
     // set initialize 
 	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		System.out.println("order selected");
-	}
+	public void initialize(URL location, ResourceBundle resources) {}
 	
 	// setTag - take productName + quantity + price = as Text
 	public void setTag(String productName, int quantity,double price) {
-		System.out.println(price);
 		itemTag.setText(productName);
 		quantityTag.setText(String.valueOf(quantity));
 		priceTag.setText(String.valueOf(price));
 	}
-	
+
 	// "Delete" RED button Clicked
 	public void delete() {
 		// take quantity,price => deduce Total
-		double sum = Integer.parseInt(quantityTag.getText()) * Double.parseDouble(priceTag.getText());
-		parentController.minusTotal(sum);
+		parentController.minusTotal(Integer.parseInt(quantityTag.getText()),Double.parseDouble(priceTag.getText()));
+		// reduce quantity in list of items
+		popupController.deleteItemFromOrderList(Integer.parseInt(quantityTag.getText()),itemTag.getText());
 		// remove HBox item
-		parentVbox.getChildren().remove(fuck);
-		
+		parentVbox.getChildren().remove(box);
 	}
 	
 }
