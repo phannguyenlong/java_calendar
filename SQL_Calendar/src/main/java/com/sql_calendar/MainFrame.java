34046b4d45664b39f71f258897c4eb50fee59a99
Long <phannguyenlong0812@gmail.com>
2021-01-26 22:02:37 +0100

    update: change app title and add appIcon

--- SQL_Calendar/src/main/java/com/sql_calendar/MainFrame.java
+++ SQL_Calendar/src/main/java/com/sql_calendar/MainFrame.java
@@ -1,35 +1,26 @@
 package com.sql_calendar;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
  * The first Frame of Application
  * @author Long Phan
  */
-public class MainFrame extends Application implements EventHandler<ActionEvent> {
+public class MainFrame extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
         Scene scene = new Scene(loader.load());
     
-        primaryStage.setTitle("Login");
+        primaryStage.setTitle("Convinient Calendar Management");
         primaryStage.setScene(scene);
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("appIcon.png")));
         // primaryStage.setMaximized(true);
         primaryStage.setResizable(false);
         primaryStage.show();
-    }  
-
-    @Override
-    public void handle(ActionEvent event) {
-        System.out.println("click the butotn");
-    }
-
-    // public static void main(String[] args) {
-    //     launch(args);
-    // }
+    } 
 }
