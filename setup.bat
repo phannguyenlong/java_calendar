5e5bbfbd0f4e504d484503afe02c6db8a09dbddb
Long <phannguyenlong0812@gmail.com>
2021-01-28 15:48:20 +0100

    update: finish setup.bat

--- setup.bat
+++ setup.bat
@@ -1,4 +1,13 @@
 copy config.properties .\SQL_Calendar\src\main\resources\com\sql_calendar\config.properties
 copy config.properties .\SQL_server\webserver\src\main\resources\config.properties
 cd ./SQL_server/webserver
-D:\stuff\apache-maven-3.6.3\bin\mvn clean package cargo:redeploy
+mvn clean package cargo:redeploy
+if errorlevel 1 (
+    D:\stuff\apache-maven-3.6.3\bin\mvn clean package cargo:redeploy
+    if errorlevel 1 (
+        @ECHO off
+        echo msgbox "Make sure you dowload apache-maven-3.6.3 in D://stuff or install maven on your workstatione",0,"Error" > #.vbs
+        #.vbs
+        del #.vbs
+    )
+)
